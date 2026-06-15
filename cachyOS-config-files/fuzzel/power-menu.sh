9c388d962f4c16f0cc209444b4d1f7b803f8ce77
frtzhahn <aldrinalciso2@gmail.com>
2026-06-15 12:01:31 +0800

    feat(fuzzel): added a script to display power menu using fuzzel

---
+++ cachyOS-config-files/fuzzel/power-menu.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+LOCK=" Lock"
+LOGOUT=" Logout"
+SUSPEND=" Suspend"
+REBOOT="\u21bb Reboot"
+SHUTDOWN=" Shutdown"
+
+CHOICE=$(echo -e "$LOCK\n$LOGOUT\n$SUSPEND\n$REBOOT\n$SHUTDOWN" | fuzzel --dmenu)
+
+case "$CHOICE" in
+    "$LOCK")
+        pgrep -x swaylock || swaylock -f --screenshots --effect-blur 7x5 --effect-vignette 0.5:0.5 --fade-in 1.5
+        ;;
+    "$LOGOUT")
+        swaymsg exit
+        ;;
+    "$SUSPEND")
+        systemctl suspend
+        ;;
+    "$REBOOT")
+        systemctl reboot
+        ;;
+    "$SHUTDOWN")
+        systemctl poweroff
+        ;;
+esac
